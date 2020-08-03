1d11f943f4b116483d9c719f0116f6721a14efa9
Mads <m.hjorth@me.com>
2020-08-03 14:26:49 +0200

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <div class="h-screen skod-bg bg-no-repeat bg-cover">
+    <div class="h-screen hero-bg bg-no-repeat bg-cover">
       <Header class="hvid" />
       <Hero />
     </div>
